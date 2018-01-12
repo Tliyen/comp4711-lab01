f81c5340d8a9e02cb8aa188bdd2b968a754ec70b
Li Yan Tong <Liyan.tong@gmail.com>
2018-01-11 21:31:26 -0800

    Update index comments

--- index.php
+++ index.php
@@ -7,14 +7,17 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>COMP4711 Lab01</title>
     </head>
     <body>
         <?php
+        // Import PHP Student class
         include('Student.php');
 
+        // Initialize array of Student objects
         $students = array();
 
+        // Add Student #1
         $first = new Student();
         $first->surname = "Doe";
         $first->first_name = "John";
@@ -25,6 +28,7 @@ and open the template in the editor.
         $first->add_grade(55);
         $students['j123'] = $first;
 
+        // Add Student #2
         $second = new Student();
         $second->surname = "Einstein";
         $second->first_name = "Albert";
@@ -36,10 +40,24 @@ and open the template in the editor.
         $second->add_grade(50);
         $students['a456'] = $second;
 
+        // Add Student #3
+        $third = new Student();
+        $third->surname = "Tong";
+        $third->first_name = "Li-Yan";
+        $third->add_email('home', 'li@home.com');
+        $third->add_email('school', 'li@bcit.ca');
+        $third->add_grade(95);
+        $third->add_grade(80);
+        $third->add_grade(90);
+        $students['b789'] = $third;
+
+        // Sort Students by first name
         ksort($students);
 
+        // Print Students on Page seperated by horizontal divider
         foreach ($students as $student) {
             echo $student->toString();
+            echo "<hr>";
         }
         ?>
     </body>
