146af42a6a608f614389db0f6fa53bab81c7030e
Li Yan Tong <Liyan.tong@gmail.com>
2018-01-11 20:41:08 -0800

    Update Student class methods to calculate average and print class info.

--- Student.php
+++ Student.php
@@ -21,7 +21,7 @@ class Student {
         $this->grades = array();
     }
 
-    //Mutator method: Add an email address
+    //Add an email address
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
@@ -29,6 +29,24 @@ class Student {
     //Add a grade
     function add_grade($grade) {
         $this->grades[] = $grade;
+    }
+
+    //Calculate and get a grade average
+    function average() {
+        $total = 0;
+        foreach ($this->grades as $value)
+            $total += $value;
+        return $total / count($this->grades);
+    }
+
+    //Print Student information
+    function toString() {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' (' . $this->average() . ")\n";
+        foreach ($this->emails as $which => $what)
+            $result .= $which . ': ' . $what . "\n";
+        $result .= "\n";
+        return '<pre>' . $result . '</pre>';
     }
 
 }
